63ce2d96c930db3351d6f8549fd5a9e3efceeffd
wzlew <wzlew@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-30 14:50:19 +0000

    update the autoAnn
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/autoann@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/autoAnn.R
+++ pkg/R/autoAnn.R
@@ -1,26 +1,25 @@
-#this is the function for predicting  posts in MOOC
-#the first input value "trainSet" is the data frame of all the train data,
-#including features we used to predict the result and the given result set.
-#the second input value "predictFormula" is formula to predict the result,
-#if it is a classification, the formula should be like : as.factor(???)~.
-#where ??? is the column name of the result to be predicted in "trainSet"
-#if it is a regression model, the formula is: ???~. where ??? is 
-#also the column name of the result to be predicted in "trainSet"
-#the third input value is "testFeature" which is data frame of all the features we used to predict the result
-#note: the column numbers of "testFeature" should be n-1 where n is the column numbers of "trainSet"
-#the fourth input value is "methodName", it is the name of method you want to use, it should be "Character"
+#input:
+#thread_id:the list of thread id
+#title: the list of thread titles
+#post_time: the list of post_time
+#post_text: the list of post_text
+#relevant_words: the list of relevant words in post text
+#key_words: the list of key words in thread titles
+#note: all these lists should have same length
+#predicted_feature: the list of features to be predicted, 
+#except the train data, the left should all be NA
+#method_name:it is the name of method you want to use, it should be "Character"
 # so if you want to use Linear SVM, the methodName should be "svmLinear".
 #the method name is the same as methods in package CARET
-#the last input value is the range,if you want to do classification, you could just ignore it
+#range:the last input value is the range,if you want to do classification, you could just ignore it
 #if you want to do regression, it should be like: c(min,max) 
 #where min is the "smallest number" and "max" is the greast number
 #the return value is a data frame contains all the predict values with column name "Result"
-autoAnn<-function(trainSet,predictFormula,testFeature,methodName,range=NULL){
-  require(caret)
-  if (is.null(range)){
-  control <- trainControl(method = "cv",savePred=T)}
-  else {control <- trainControl(method = "cv",savePred=T,predictionBounds=range)}
-  model<-train(predictFormula, data=trainSet, method = methodName, trControl = control)
-  Result<-predict(model,newdata=testFeature)
-  Result<-data.frame(Result)
-  return(Result)}
+
+autoAnn<-function(thread_id,title,post_time,post_text,relevant_words,key_words,predicted_feature,method_name,range=NULL){
+        result<-featureExtract(thread_id,title,post_time,post_text,relevant_words,key_words)
+        rowNum<-which(!is.na(predicted_feature))
+        trainSet<-data.frame(result[rowNum,],predicted_feature[rowNum])
+        colnames(trainSet)[ncol(trainSet)]<-"predict"
+        predictResult<-featurePredict(trainSet,predict~.,result,method_name,range)
+}
